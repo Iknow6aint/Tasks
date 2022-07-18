4118330203ee040ec9aa3569dc329c6bcadadb57
saint <saintmoses100@gmail.com>
2022-07-18 10:08:36 -0300

    correct task 1

--- task1.js
+++ task1.js
@@ -1,8 +1,8 @@
 function getSum(arr){
     const n = arr.length
-    let even = 0;
     let odd = 0;
     for (let i = 0; i < n ; i++) {
+        
         if (i%2 ==0) {
             even += arr[i]
         } else {
@@ -13,7 +13,9 @@ function getSum(arr){
  
 }
 
-let arr = [1,2,3,4,5,6];
+let arr = [1,2,3,4,5,6,'saint'];
+const filter = arr.filter(num => num > 0 && num % parseInt(num) === 0)
+let even = 0;
 
-getSum(arr);
+getSum(filter);
 
